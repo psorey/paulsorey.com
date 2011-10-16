cc889ad244bb037ec6ff578a18a8052a1f0d0041
Paul Sorey <psorey@comcast.net>
2011-10-15 19:01:54 -0700

    added rain drums content

--- paulsorey.rb
+++ paulsorey.rb
@@ -1,17 +1,13 @@
 require 'sinatra'
 require 'erb'
 require 'sinatra/outputbuffer'
-#require 'sinatra/content_for'
 require 'haml'
 require 'sass'
 
-#
-#Sinatra::Templates.initialize()
 
 get '/' do
   erb :index
 end
-
 
 get '/mystyle.css' do
   scss :mystyle
@@ -45,16 +41,13 @@ get '/SalmonWaves' do
   erb :salmon
 end
 
-
 get '/FractalTreeArchway' do
   erb :skagit
 end
 
-
 get '/RoadWorks' do
   erb :pierce
 end
-
 
 get '/Flock' do
   erb :flock
@@ -64,11 +57,9 @@ get '/CoffeePotPergola' do
   erb :coffee
 end
 
-
 get '/TreeBench' do
   erb :treeBench
 end
-
 
 get '/JusticeGardenPath' do
   erb :justice
@@ -78,11 +69,9 @@ get '/CribbageBenches' do
   erb :cribbage
 end
 
-
 get '/PacificFountain' do
   erb :pacific
 end
-
 
 get '/DogwoodFountain' do
   erb :dogwood
@@ -92,26 +81,21 @@ get '/AuburnFigures' do
   erb :auburn
 end
 
-
 get '/LongfellowCreek' do
   erb :longfellow
 end
-
 
 get '/SustainabilityArtwork' do
   erb :sustainability
 end
 
-
 get '/FlowerSculpture' do
   erb :horns
 end
 
-
 get '/TreeBenchVideo' do
   erb :treeBenchVideo
 end
-
 
 get '/ArtworkLocations' do
   erb :locations
